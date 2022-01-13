a46df7a87c368ed52a5be5e842cd5083f1ad6dc3
navasjl <94189105+navasjl@users.noreply.github.com>
2022-01-13 09:28:13 +0100

    Update BootcamperService.java

--- Ejercicio 3_4_5/demoSpringRest/src/main/java/com/openbootcamp/demoSpringRest/services/BootcamperService.java
+++ Ejercicio 3_4_5/demoSpringRest/src/main/java/com/openbootcamp/demoSpringRest/services/BootcamperService.java
@@ -1,7 +1,7 @@
 package com.openbootcamp.demoSpringRest.services;
 
 import com.openbootcamp.demoSpringRest.models.Bootcamper;
-import org.jvnet.hk2.annotations.Service;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
